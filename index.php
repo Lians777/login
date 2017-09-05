1d5b2cb3da3b17147dfb87e7be04e06991a9121c
Lians777 <sungaila.linas@gmail.com>
2017-09-05 21:24:54 +0300

    mai

--- index.php
+++ index.php
@@ -1,32 +1,49 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"
-	<title></title>
-</head>
-<body>
-<div class="container">
-	
-	<div class="row">
-		<div class="col"></div>
-		<div class="col"></div>
-	</div>
-</div>
-	
-
-
-
-
-
-
 <?php 
+
+session_start();
+
+
+if (isset($_POST['username']) && (isset($_POST['password'])) {
+
+    if ($_POST['username'] == "ignas" && $_POST['passworld'] == "slaptas") {
+        
+        $_SESSION['user']['usernaem'] = $_POST['usernaem'];
+       	$_SESSION['user']['level'] = $_POST['admin'];
+
+
+ } else {
+ $error = "wrong usernaem or passworld";
+    }
+
+}
+
+
+    
+else if && (isset($_POST['submit'])
+
+
+
+
+if (isset($_GET['logout'])) {
+	session_destroy();
+
+}
+   
+
+
+
+
+ else {
+$error = "no login";
+}
+
+if (isset($_SESSION['user'])) {
+    
+}$vardas = $_SESSION['user']['usernaem'];
+
 
 
 
 include "main.php";
+
  ?>
-
-
-
-</body>
-</html>
--- main.php
+++ main.php
@@ -1,1 +1,72 @@
-{\rtf1}
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"
+	<title></title>
+</head>
+<body>
+<div class="container">
+	<h1>login</h1>
+	<div class="row">
+
+		<div class="col-5">
+
+
+		 </div>
+	
+
+
+		
+			<form method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">username</label>
+    <input name="username" type="email" class="form-control" id="username" aria-describedby="username" placeholder="username">
+  </div>
+  <div class="form-group">
+    <label for="Password">Password</label>
+    <input name="password" type="password" class="form-control" id="Password" placeholder="Password">
+  </div>
+  <button name="submit" type="submit" class="btn btn-primary">login</button>
+</form>
+
+
+		</div>
+		<div class="col">
+			<pre>  
+			 (<a href="logout=true">logout</a>)
+			<?php 
+		
+
+			if (condition) {
+				# code...
+			}
+
+
+				echo "POST:";
+
+				print_r($_POST);
+
+				echo "SESSION:";
+
+				print_r($_SESSION);
+
+				echo "COOKIE:";
+
+				print_r($_COOKIE);
+
+
+
+			 ?>
+
+			</pre>
+
+		</div>
+	</div>
+</div>
+	
+
+
+
+
+</body>
+</html>
