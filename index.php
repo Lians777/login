940c918fe85248d1b1eae17a624b3ae7b2547cbe
Lians777 <sungaila.linas@gmail.com>
2017-09-05 20:08:43 +0300

    login

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"
+	<title></title>
+</head>
+<body>
+<div class="container">
+	
+	<div class="row">
+		<div class="col"></div>
+		<div class="col"></div>
+	</div>
+</div>
+	
+
+
+
+
+
+
+<?php 
+
+
+
+include "main.php";
+ ?>
+
+
+
+</body>
+</html>
